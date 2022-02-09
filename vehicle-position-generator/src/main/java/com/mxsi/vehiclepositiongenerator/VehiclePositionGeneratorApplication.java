307f9150df2a8dbfa7c587c22c47874775d36e65
Max Shannon <maxshannon2016@hotmail.com>
2022-02-09 18:47:44 +0000

    Initial commit

---
+++ vehicle-position-generator/src/main/java/com/mxsi/vehiclepositiongenerator/VehiclePositionGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.mxsi.vehiclepositiongenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclePositionGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiclePositionGeneratorApplication.class, args);
+	}
+
+}
---
+++ vehicle-position-generator/src/test/java/com/mxsi/vehiclepositiongenerator/VehiclePositionGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mxsi.vehiclepositiongenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclePositionGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
